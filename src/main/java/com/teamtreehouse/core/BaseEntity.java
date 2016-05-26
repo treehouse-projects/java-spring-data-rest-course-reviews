f7f47c1d565ae75fbcd6ce0b47278cb6c1e149da
Craig Dennis <craig@teamtreehouse.com>
2016-05-26 14:02:52 -0700

    Adds versioning

--- src/main/java/com/teamtreehouse/core/BaseEntity.java
+++ src/main/java/com/teamtreehouse/core/BaseEntity.java
@@ -1,15 +1,14 @@
 package com.teamtreehouse.core;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 @MappedSuperclass
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private final Long id;
+    @Version
+    private Long version;
 
     protected BaseEntity() {
         id = null;
