09ee8e9b841be51b8c81f40614f65255076d0934
Craig Dennis <craig@teamtreehouse.com>
2016-05-26 18:05:29 -0700

    Adds delete authorization

--- src/main/java/com/teamtreehouse/review/ReviewRepository.java
+++ src/main/java/com/teamtreehouse/review/ReviewRepository.java
@@ -2,6 +2,15 @@ package com.teamtreehouse.review;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface ReviewRepository extends PagingAndSortingRepository<Review, Long> {
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @reviewRepository.findOne(#id)?.reviewer?.username == authentication.name")
+    void delete(@Param("id") Long id);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #review.reviewer?.username == authentication.name")
+    void delete(@Param("review") Review entity);
 }
